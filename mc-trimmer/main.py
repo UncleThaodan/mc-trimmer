ccf6b694393e5598d9510b2e75a9a7bd9ecdbfb8
Thaodan <49715681+UncleThaodan@users.noreply.github.com>
2023-07-23 17:33:54 +0200

    Read/write chunks (without regard for NBT)

--- mc-trimmer/main.py
+++ mc-trimmer/main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import struct
 from typing import Any, Callable, Iterable, Self, Type, TypeVar
 from enum import IntEnum
+import zlib
 
 
 PATH = "./data"
@@ -14,6 +15,8 @@ T = TypeVar('T', bound='Serializable')
 class Sizes(IntEnum):
     CHUNK_LOCATION_DATA_SIZE = 4 * 1024
     TIMESTAMPS_DATA_SIZE = 4 * 1024
+    CHUNK_SIZE_MULTIPLIER = 4 * 1024
+    CHUNK_HEADER_SIZE = 5
 
 
 class Meta(type):
@@ -66,6 +69,9 @@ class ChunkLocation(Serializable):
         self.offset = offset
         self.size = size
 
+    def __repr__(self) -> str:
+        return str(f'{self.__class__}: {self.offset}+{self.size}')
+
     @classmethod
     def from_bytes(cls, data: bytes) -> Self:
         offset, size = struct.unpack(">IB", b'\x00' + data)  # 3 byte offset, 1 byte size
@@ -86,6 +92,9 @@ class Timestamp(Serializable):
     def __init__(self, timestamp: int) -> None:
         self.timestamp = timestamp
 
+    def __repr__(self) -> str:
+        return str(f'{self.__class__}: {self.timestamp}')
+
     @classmethod
     def from_bytes(cls, data: bytes) -> Self:
         timestamp, = struct.unpack(">I", data)
@@ -98,6 +107,38 @@ class Timestamp(Serializable):
     @property
     def SIZE(cls) -> int:
         return 4
+
+class Chunk(Serializable):
+    def __init__(self, len: int = 0, compression: int = 2, data: bytes = b'') -> None:
+        self._len: int = len
+        self._compression: int = compression
+        self._unpacked_data: bytes = data
+
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        len, compression = struct.unpack(">IB", data[:Sizes.CHUNK_HEADER_SIZE])
+        assert compression == 2
+        dd = zlib.decompress(data[Sizes.CHUNK_HEADER_SIZE : Sizes.CHUNK_HEADER_SIZE + len])
+        return cls(len=len, compression=compression, data=dd)
+
+    def conditional_reset(self, condition: Callable[[Self], bool]):
+        if condition(self):
+            self._len = 0
+            self._unpacked_data = b''
+
+    def __bytes__(self) -> bytes:
+        if self._unpacked_data != b'':
+            compressed_data = zlib.compress(self._unpacked_data)
+            data = struct.pack(">IB", len(compressed_data), self._compression) + compressed_data
+            self._len = len(data)
+            return data
+        return b''
+
+    @property
+    def SIZE(self) -> int:
+        return Sizes.CHUNK_HEADER_SIZE + self._len
+
 
 print(Timestamp.__dict__)
 
@@ -113,9 +154,10 @@ def get_regions(path: str | Path) -> Iterable[Path]:
 
 
 class Region:
-    def __init__(self, chunk_location_data: bytes, timestamps_data: bytes) -> None:
+    def __init__(self, chunk_location_data: bytes, timestamps_data: bytes, data: bytes) -> None:
         self.__locations = ChunkLocationData().from_bytes(chunk_location_data)
         self.__timestamps = TimestampData().from_bytes(timestamps_data)
+        self.__data = data
 
         # Test:
         a = bytes(self.__locations)
@@ -126,13 +168,27 @@ class Region:
         b = bytes(timestamps_data)
         assert(b == a)
 
+        self.__chunks: list[Chunk] = []
+        for loc in self.__locations:
+            if loc.size == 0:
+                self.__chunks.append(Chunk())
+            else:
+                # location is relative to beginning of file, so timestamp and location table have to be subtracted.
+                self.__chunks.append(Chunk.from_bytes(self.__data[loc.offset * Sizes.CHUNK_SIZE_MULTIPLIER - Sizes.CHUNK_LOCATION_DATA_SIZE - Sizes.TIMESTAMPS_DATA_SIZE: ]))
+        pass
+
+    def trim(self, condition: Callable[[Chunk], bool]):
+        for c in self.__chunks:
+            c.conditional_reset(condition)
+
+
 
 def open_region(region: Path):
     with open(region, "+rb") as f:
         chunk_location_data: bytes = memoryview(f.read(Sizes.CHUNK_LOCATION_DATA_SIZE))
         timestamps_data: bytes = memoryview(f.read(Sizes.TIMESTAMPS_DATA_SIZE))
 
-        Region(chunk_location_data, timestamps_data)
+        a = Region(chunk_location_data, timestamps_data, memoryview(f.read()))
         pass
 
 
